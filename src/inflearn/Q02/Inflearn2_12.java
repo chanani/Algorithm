2331d2c6a82de0a3261cbe778e2cdbe536b6fd35
CHANHAN <130114269+chanani@users.noreply.github.com>
2023-12-18 22:16:41 +0900

    Inflearn2-12

---
+++ src/inflearn/Q02/Inflearn2_12.java
@@ -0,0 +1,48 @@
+package inflearn.Q02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Inflearn2_12 {
+    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/03-1
+    public static int solution(int student, int test, int[][] arr) {
+        int answer = 0;
+
+        for (int i = 1; i <= student; i++) {
+            for (int j = 1; j <= student; j++) {
+                int count = 0;
+                for (int k = 0; k < test; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < student; s++) {
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
+                    }
+                    if (pi < pj) count++;
+                }
+                if (count == test) answer++;
+            }
+
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int student = scan.nextInt();
+        int test = scan.nextInt();
+        int[][] arr = new int[test][student];
+        for (int i = 0; i < test; i++) {
+            for (int j = 0; j < student; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println(solution(student, test, arr));
+
+
+    }
+}
--- src/inflearn/Q03/Inflearn3_1.java
+++ src/inflearn/Q03/Inflearn3_1.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 
 public class Inflearn3_1 {
 
-    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/02-11
+    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/03-1
     // 내가 작성한 코드
-    public static ArrayList<Integer> solution(int[] arr1, int[] arr2) {
+   /* public static ArrayList<Integer> solution(int[] arr1, int[] arr2) {
         ArrayList<Integer> answer = new ArrayList<>();
         for (int i = 0; i < arr1.length; i++) {
             answer.add(arr1[i]);
@@ -18,6 +18,18 @@ public class Inflearn3_1 {
             answer.add(arr2[i]);
         }
         Collections.sort(answer);
+
+        return answer;
+    }*/
+    public static ArrayList<Integer> solution(int n1, int n2, int[] arr1, int[] arr2) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while(p1 < n1 && p2 < n2){
+            if (arr1[p1] < arr2[p2]) answer.add(arr1[p1++]);
+            else answer.add(arr2[p2++]);
+        }
+        while(p1< n1) answer.add(arr1[p1++]);
+        while(p2< n2) answer.add(arr2[p2++]);
 
         return answer;
     }
@@ -35,7 +47,7 @@ public class Inflearn3_1 {
             arr2[i] = scan.nextInt();
         }
 
-        for(int x : solution(arr1, arr2)){
+        for(int x : solution(n1, n2, arr1, arr2)){
             System.out.print(x + " ");
         }
     }
