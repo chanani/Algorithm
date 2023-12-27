8d9a59bd34ac1b50fa92cb3ab7f79b1bed3b8c24
CHANHAN <130114269+chanani@users.noreply.github.com>
2023-12-27 18:14:14 +0900

    inflearn5-1~2

---
+++ src/inflearn/q05_stack_queue/Inflearn5_2.java
@@ -0,0 +1,26 @@
+package inflearn.q05_stack_queue;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.TreeSet;
+
+public class Inflearn5_2 {
+    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/05-02
+    // 내가 작성한 코드
+
+    public static String solution(String str) {
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(solution(str));
+    }
+}
