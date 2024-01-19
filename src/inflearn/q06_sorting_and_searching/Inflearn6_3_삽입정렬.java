9f7035ea4349479b8eecc48d0bf6539dcc07d8eb
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-01-20 01:06:08 +0900

    inflearn6_3 삽입정렬

--- src/inflearn/q06_sorting_and_searching/Inflearn6_3_삽입정렬.java
+++ src/inflearn/q06_sorting_and_searching/Inflearn6_3_삽입정렬.java
@@ -2,22 +2,18 @@ package inflearn.q06_sorting_and_searching;
 
 import java.util.Scanner;
 
-public class Inflearn6_2_버블정렬 {
+public class Inflearn6_3_삽입정렬 {
+    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/06_3
 
-    // 문제 링크 : https://cote.inflearn.com/contest/10/problem/06_2
-    // 내가 작성한 코드
     public static int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                 int tmp = arr[j];
-                 arr[j] = arr[j + 1];
-                 arr[j + 1] = tmp;
-                }
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0 ; j--) {
+                if (arr[j] > tmp) arr[j + 1] = arr[j];
+                else break;
             }
+            arr[j + 1] = tmp;
         }
-
-
         return arr;
     }
 
