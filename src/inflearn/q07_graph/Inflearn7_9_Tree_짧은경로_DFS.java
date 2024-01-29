170f56daf2b0f12f9679da7b83d1cf65b6a0f015
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-01-29 22:35:48 +0900

    Inflearn7-9 Tree 말단노드까지 가장 짧은 경로(DFS) : 해당 문제는 BFS로 해결하는 것이 정석

---
+++ src/inflearn/q07_graph/Inflearn7_9_Tree_짧은경로_DFS.java
@@ -0,0 +1,21 @@
+package inflearn.q07_graph;
+
+public class Inflearn7_9_Tree_짧은경로_DFS {
+
+    static Node root;
+    public static int DFS(int L, Node root){
+        if (root.lt == null && root.rt == null) return L;
+        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    }
+
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        System.out.println(DFS(0, root));
+
+    }
+}
